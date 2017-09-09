ea529eec8adde0d5cdd8b14a62c7fe682842a436
Sid <gadkari.stuff@gmail.com>
2017-09-09 05:30:30 -0500

    Update clock.py

--- clock.py
+++ clock.py
@@ -2,7 +2,7 @@
 # Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met: * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution. * Neither the name of the nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-# Last Revision Date: 08/30/2017
+# Last Revision Date: 09/08/2017
 
 #---------------------BEGIN USER PREFERENCES---------------------
 # Define time format (12 or 24 hour)
@@ -13,6 +13,9 @@ show_weather = "yes"
 
 # Define temperature unit (F or C)
 unit_pref = "F"
+
+# Define your Weather Underground API key
+api_key = "ADD API KEY HERE"
 
 # Define your US zip code
 zipcode = "20001"
@@ -108,7 +111,7 @@ def currenttime():
 		hour = (now.hour - hour_offset)
 	
 	# Define condition to get current temperature
-	if (second == 1) and (((minute % 10) == 0) or ((minute % 10) == 2) or ((minute % 10) == 4)  or ((minute % 10) == 6) or ((minute % 10) == 8)):
+	if (second == 1) and (((minute % 10) == 3) or ((minute % 10) == 6)  or ((minute % 10) == 9)):
 		flag_getweather = "true"
 
 	# Deine condition to display the temperature 4 times a minute
@@ -129,10 +132,11 @@ def weatherupdate():
 	os.system("rm weatherdata.tmp")
 
 	# Create URL for weather lookup
-	url = "https://www.wunderground.com/cgi-bin/findweather/getForecast?query=" + zipcode
+	url = "http://api.wunderground.com/api/" + api_key + "/conditions/q/" + zipcode + ".json"
 	
 	# Generate weather update command
 	weatherupdate = "curl -A 'Mozilla/5.0' -L -o weatherdata.tmp " + url
+	
 	# Temperature current data
 	os.system(weatherupdate)
 
@@ -253,7 +257,7 @@ while(True):
 	# First run behavior
 	if (auto_dimming == "enabled"):
 		if ((minute == 0) and (second <= 10)) or (foo == 0):
-			if ((hour >= 8) and (hour <= 15)):
+			if ((hour >= 7) and (hour <= 18)):
 				LEDBackpack().setBrightness(day_bright)
 			else:
 				LEDBackpack().setBrightness(night_bright)
