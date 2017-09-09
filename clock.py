7e89499be69c4c87b51af841bfc527d182225a3c
Sid <gadkari.stuff@gmail.com>
2017-09-09 05:51:16 -0500

    Update clock.py

--- clock.py
+++ clock.py
@@ -2,7 +2,7 @@
 # Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met: * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution. * Neither the name of the nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-# Last Revision Date: 09/08/2017
+# Last Revision Date: 09/09/2017
 
 #---------------------BEGIN USER PREFERENCES---------------------
 # Define time format (12 or 24 hour)
@@ -150,8 +150,9 @@ def weatherupdate():
 			raw_temp = open('current_temp.tmp', 'r').read()
 	
 			# Convert temperature value to an integer
-			current_temp = int(float(raw_temp))
-	
+			float_temp = float(raw_temp)
+			current_temp = int(round(float_temp))
+			
 			# Handle temperature based on user preference (default is F)
 			if (unit_pref == "C"):
 				conversion = ((current_temp - 32) / 1.8)
